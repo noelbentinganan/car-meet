58d2614217dbabeac89e38f4e3ff134f48b88d57
John Noel <jnoelbentinganan@gmail.com>
2025-02-20 14:37:51 +1100

    signin and signup UI done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -18,8 +18,9 @@ const SignUp = () => {
     address: "",
     email: "",
     password: "",
+    password1: "",
   });
-  const { firstName, lastName, address, email, password } = formData;
+  const { firstName, lastName, address, email, password, password1 } = formData;
 
   const navigate = useNavigate();
 
@@ -42,107 +43,110 @@ const SignUp = () => {
       // Initialize getAuth fn
       const auth = getAuth();
 
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+      // Password validation
+      if (password !== password1) {
+        console.log("Password does not match.");
+      } else {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
 
-      // ! For database needs
-      const user = userCredential.user;
+        // ! For database needs
+        const user = userCredential.user;
 
-      updateProfile(auth.currentUser, {
-        displayName: firstName,
-      });
+        updateProfile(auth.currentUser, {
+          displayName: firstName,
+        });
 
-      // Save to firestore process:
-      // 1.
-      const formDataCopy = { ...formData }; // Copy all formData
-      delete formDataCopy.password; // Remove the password so that it will not save to firestore
-      formDataCopy.timestamp = serverTimestamp(); // Add timestamp property to formDataCopy object and use serverTimestamp
+        // Save to firestore process:
+        // 1.
+        const formDataCopy = { ...formData }; // Copy all formData
+        delete formDataCopy.password && delete formDataCopy.password1; // Remove the password so that it will not save to firestore
+        formDataCopy.timestamp = serverTimestamp(); // Add timestamp property to formDataCopy object and use serverTimestamp
 
-      // 2.
-      await setDoc(doc(db, "users", user.uid), formDataCopy);
+        // 2.
+        await setDoc(doc(db, "users", user.uid), formDataCopy);
 
-      console.log(formDataCopy);
+        console.log(formDataCopy);
 
-      navigate("/user-profile");
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="su-form-container">
-      <p>Complete your details to join the meet.</p>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="First name"
-          id="firstName"
-          value={firstName}
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="Last name"
-          id="lastName"
-          value={lastName}
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="Address Line 1"
-          id="addressLine1"
-          value={address}
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="Address Line 2"
-          id="addressLine2"
-          value={address}
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="City"
-          id="city"
-          value={address}
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="State / Province / Region"
-          id="state"
-          value={address}
-          onChange={onChange}
-        />
-        <input
-          type="number"
-          placeholder="State / Province / Region"
-          id="state"
-          value={address}
-          onChange={onChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          id="email"
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          value={password}
-          onChange={onChange}
-        />
+    <div className="container">
+      <section className="signup-form container-m signup-form-m">
+        <p className="text-md text-600 my-3">User Information</p>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="First name"
+            id="firstName"
+            value={firstName}
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            id="lastName"
+            value={lastName}
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            id="address"
+            value={address}
+            onChange={onChange}
+          />
 
-        <button>Sign Up</button>
-      </form>
-      <GoogleOAuth />
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            id="password1"
+            value={password1}
+            onChange={onChange}
+          />
+
+          {password !== password1 ? (
+            <p className="password-confirm">
+              Password does not match, please try again.
+            </p>
+          ) : (
+            ""
+          )}
+
+          <button className="signup-btn">Sign Up</button>
+        </form>
+
+        <p className="text-xs text-600">Or</p>
+        <GoogleOAuth />
+        <p className="text-xs">
+          Already have an account?{" "}
+          <Link className="a-link" to="/sign-in">
+            Sign In
+          </Link>
+        </p>
+      </section>
     </div>
   );
 };
